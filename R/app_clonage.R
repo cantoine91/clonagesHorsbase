368e2e240ccb5567540d22245195556725713e5b
Chloe Antoine <cantoine@hybrigenics.com>
2025-06-25 11:04:08 +0200

    fichiers

--- R/app_clonage.R
+++ R/app_clonage.R
@@ -15,7 +15,7 @@ ui <- fluidPage(
     # Application title
     titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             sliderInput("bins",
@@ -47,5 +47,5 @@ server <- function(input, output) {
     })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
