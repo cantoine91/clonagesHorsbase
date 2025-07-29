c57ed73e21db0f8b6d2b78508e6dbc9565d64992
Chloe Antoine <cantoine@hybrigenics.com>
2025-07-29 17:43:52 +0200

    config en premier

--- R/app_clonage.R
+++ R/app_clonage.R
@@ -1,8 +1,8 @@
 # Inclure les scripts
+source("config_clonage.R")
 source("global_clonage.R")
 source("ui_clonage.R")
 source("server_clonage.R")
-source("config_clonage.R")
 
 # Lancer l'application
 shinyApp(ui = ui_clonage, server = server_clonage)
